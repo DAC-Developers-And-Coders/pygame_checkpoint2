75e2eaf60870facc9b31b7504539677db420458c
André Victor Gonçalves Nascimento <135927008+AndreVictoN@users.noreply.github.com>
2026-05-14 01:08:17 -0300

    Adicionados: botão de sair do jogo e cenários

--- GameManager.py
+++ GameManager.py
@@ -1,14 +1,26 @@
 import pygame
+import sys
 
-def verificar_sair_menu(tecla_pressionada = None, evento = None):
+def verificar_iniciar_jogo(tecla_pressionada = None, evento = None):
     if evento is not None:
-        x = evento.pos[0]
-        y = evento.pos[1]
-
-        if 605 < x < 695 and 321 < y < 360:
+        x, y = evento.pos
+        if 882 < x < 1028 and 543 < y < 586:
             return False
 
     if tecla_pressionada is not None and tecla_pressionada[pygame.K_RETURN]:
         return False
 
     return True
+
+def verificar_sair_jogo(evento):
+    if evento is not None:
+        x, y = evento.pos
+        if 902 < x < 1007 and 603 < y < 647:
+            pygame.quit()
+            sys.exit()
+
+def verificar_tecla_pressionada(tecla_pressionada, verificando_retorno_menu):
+    if tecla_pressionada[pygame.K_ESCAPE] and verificando_retorno_menu:
+        return True
+
+    return None
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import pygame
 import sys
 
-from GameManager import verificar_sair_menu
+from GameManager import verificar_tecla_pressionada
+from GameManager import verificar_iniciar_jogo
+from GameManager import verificar_sair_jogo
 
 pygame.init()
 
 #CONFIG GERAL
-LARGURA = 1280
-ALTURA = 720
+LARGURA = 1920
+ALTURA = 1080
 BRANCO = (255,255,255)
 PRETO = (0,0,0)
 
@@ -17,13 +19,15 @@ pygame.display.set_caption("CP2")
 
 clock = pygame.time.Clock()
 fonte_menu = pygame.font.Font('./fonts/NightsideDemoRegular.ttf', 100)
-fonte_geral = pygame.font.Font('./fonts/DevilCandle.otf', 30)
+fonte_geral = pygame.font.Font('./fonts/DevilCandle.otf', 50)
 
 texto_menu = fonte_menu.render("FAITH", True, BRANCO)
 botao_jogar = fonte_geral.render("Jogar", True, BRANCO)
+botao_sair = fonte_geral.render("Sair", True, BRANCO)
 
 #IMAGENS
-menu_image = pygame.image.load("./images/house-menu.jpeg").convert()
+menu_image = pygame.image.load("./images/house-exterior.png").convert()
+hall_entrada = pygame.image.load("./images/entrance-hall.png").convert()
 
 no_menu = True
 
@@ -34,17 +38,23 @@ while True:
             sys.exit()
 
         if no_menu and evento.type == pygame.MOUSEBUTTONDOWN:
-            no_menu = verificar_sair_menu(None, evento)
+            no_menu = verificar_iniciar_jogo(None, evento)
+
+            if no_menu:
+                verificar_sair_jogo(evento)
 
     tecla_pressionada = pygame.key.get_pressed()
 
     if no_menu:
-        no_menu = verificar_sair_menu(tecla_pressionada)
-        tela.blit(menu_image, (24,0))
-        tela.blit(texto_menu, (550, 160))
-        tela.blit(botao_jogar, (605, 330))
-    else:
+        no_menu = verificar_iniciar_jogo(tecla_pressionada)
         tela.fill(PRETO)
+        tela.blit(menu_image, (236,0))
+        tela.blit(texto_menu, (860, 400))
+        tela.blit(botao_jogar, (880, 540))
+        tela.blit(botao_sair, (900, 600))
+    else:
+        no_menu = verificar_tecla_pressionada(tecla_pressionada, True)
+        tela.blit(hall_entrada, (236, 0))
 
     pygame.display.flip()
     clock.tick(60)
